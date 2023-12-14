b1dce8eb45932747567d8469f75568c9a679cd8a
Mojtaba Pakzad <mj.pakzad@outlook.com>
2023-12-14 17:04:14 +0330

    Add TransactionGateway enum

---
+++ app/Enums/TransactionGateway.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Enums;
+
+use ArchTech\Enums\InvokableCases;
+use ArchTech\Enums\Options;
+use ArchTech\Enums\Values;
+
+enum TransactionGateway: int
+{
+    use InvokableCases;
+    use Values;
+    use Options;
+
+    case CREDIT = 1;
+
+    case MELLAT = 2;
+
+    case ZARINPAL = 3;
+
+    case PAYLINE = 4;
+
+    case JAHANPAY = 5;
+
+    case PARSIAN = 6;
+
+    case PASARGAD = 7;
+
+    case SAMAN = 8;
+
+    case ASANPARDAKHT = 9;
+
+    case PAYPAL = 10;
+
+    case PAYIR = 11;
+
+    case SADAD = 14;
+}
