3d52e374463a78cdf974a689b8c7b832229e3f2a
Mojtaba Pakzad <mj.pakzad@outlook.com>
2023-12-14 17:07:40 +0330

    Add orders migration

---
+++ database/migrations/2023_12_14_082843_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->text('content')->nullable();
+            $table->unsignedSmallInteger('status')->default(OrderStatus::PAYMENT_PENDING->value);
+            $table->unsignedBigInteger('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
