5575ec092a982807775dba9bd01439b5a1d3e1ca
Mojtaba Pakzad <mj.pakzad@outlook.com>
2023-12-14 17:10:34 +0330

    Add Product model

---
+++ app/Models/Product.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ProductStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'heading',
+        'slug',
+        'content',
+        'stock',
+        'price',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'stock' => 'integer',
+        'price' => 'integer',
+        'status' => ProductStatus::class,
+    ];
+
+    /**
+     * Get the route key for the model.
+     */
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    /**
+     * Get orders associated with the products.
+     *
+     * @return BelongsToMany
+     */
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class)->withPivot(['quantity', 'amount', 'total_amount']);
+    }
+}
