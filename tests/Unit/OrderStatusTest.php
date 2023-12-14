0dee53c100722d9b41217d431c193ae061264c41
Mojtaba Pakzad <mj.pakzad@outlook.com>
2023-12-14 17:03:41 +0330

    Add OrderStatus enum test

---
+++ tests/Unit/OrderStatusTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Enums\OrderStatus;
+use Tests\TestCase;
+
+class OrderStatusTest extends TestCase
+{
+    /** @test */
+    public function order_has_expected_statuses(): void
+    {
+        $statuses = [
+            'PAYMENT_PENDING' => 10,
+            'PAID' => 20,
+            'COLLECTING' => 30,
+            'SENT' => 40,
+            'HANDED_OVER_TO_COURIER' => 50,
+            'DELIVERED' => 60,
+            'PAYMENT_FAILED' => 70,
+            'FAILED' => 80,
+            'CANCELED' => 90,
+        ];
+        $this->assertEquals($statuses, OrderStatus::options());
+    }
+}
