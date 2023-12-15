3b8ba4d023306244b5c2d93a243068c3ac462943
Mojtaba Pakzad <mj.pakzad@outlook.com>
2023-12-15 13:56:20 +0330

    Format Authentication Exception response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -25,6 +26,9 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function (AuthenticationException $e, $request) {
+            throw_if ($request->is('api/*'), AuthentionFaildException::class);
         });
     }
 }
